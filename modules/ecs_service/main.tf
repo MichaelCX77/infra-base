7c03aa811c2c3345bcb9ea0495aa5a01988f5bba
Michael Xavier <maycow_77@hotmail.com>
2025-06-14 15:39:04 -0300

    feat: adicao de lifecicle para sobrescrever recursos ja existentes

--- modules/ecs_service/main.tf
+++ modules/ecs_service/main.tf
@@ -1,4 +1,11 @@
-# Pega a VPC existente pelo nome da tag "Name"
+# Gera sufixo aleatório para evitar conflito de nomes
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+# VPC existente pelo nome da tag
 data "aws_vpcs" "myvpc" {
   filter {
     name   = "tag:Name"
@@ -10,9 +17,10 @@ data "aws_vpc" "myvpc" {
   id = data.aws_vpcs.myvpc.ids[0]
 }
 
+# Zonas de disponibilidade
 data "aws_availability_zones" "available" {}
 
-
+# Subnets públicas com tag "Public = true"
 data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
@@ -25,6 +33,7 @@ data "aws_subnets" "public" {
   }
 }
 
+# Security group para o ALB
 resource "aws_security_group" "alb_sg" {
   vpc_id = data.aws_vpc.myvpc.id
 
@@ -41,24 +50,39 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
+# Load Balancer com nome único
 resource "aws_lb" "this" {
-  name               = var.project_name
+  name               = "${var.project_name}-${random_string.suffix.result}"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
   subnets            = data.aws_subnets.public.ids
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
+# Target group
 resource "aws_lb_target_group" "this" {
   name        = "${var.project_name}-tg"
   port        = var.container_port
   protocol    = "HTTP"
   vpc_id      = data.aws_vpc.myvpc.id
   target_type = "ip"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
+# Listener HTTP
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
@@ -70,14 +94,26 @@ resource "aws_lb_listener" "http" {
   }
 }
 
+# ECR repository protegido contra destruição
 resource "aws_ecr_repository" "repo" {
   name = var.project_name
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [image_tag_mutability, encryption_configuration]
+  }
 }
 
+# ECS cluster
 resource "aws_ecs_cluster" "this" {
   name = "${var.project_name}-cluster"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
+# ECS Task Definition
 resource "aws_ecs_task_definition" "this" {
   family                   = "${var.project_name}-task"
   requires_compatibilities = ["FARGATE"]
@@ -95,6 +131,7 @@ resource "aws_ecs_task_definition" "this" {
   }])
 }
 
+# ECS Service
 resource "aws_ecs_service" "this" {
   name            = "${var.project_name}-service"
   cluster         = aws_ecs_cluster.this.id
