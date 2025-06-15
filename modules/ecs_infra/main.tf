09629ae51b5f5049a1ea8054e451a679b1d643ff
Michael Xavier <maycow_77@hotmail.com>
2025-06-15 15:41:25 -0300

    bug: ajuste da porta do container

--- modules/ecs_infra/main.tf
+++ modules/ecs_infra/main.tf
@@ -74,7 +74,7 @@ resource "aws_lb_target_group" "this" {
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
-  port              = 8080
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
